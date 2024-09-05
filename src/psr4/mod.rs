6008cfad66e74113ed1001848d9acf98a341560f
tu6ge <wanghonglei@jbinfo.cn>
2024-09-05 14:40:21 +0800

    feat: seralize autoload_files file

--- src/psr4/mod.rs
+++ src/psr4/mod.rs
@@ -14,3 +14,17 @@ pub(crate) struct Psr4Data {
 pub(crate) struct FilesData {
     data: IndexMap<String, (IsVendor, String)>,
 }
+
+impl FilesData {
+    pub fn insert(&mut self, is_vendor: IsVendor, value: String) -> Option<(IsVendor, String)> {
+        use sha1::Digest;
+        use sha1::Sha1;
+
+        let mut hasher = Sha1::new();
+        hasher.update(&value.as_bytes());
+        let sha1 = hasher.finalize();
+        let key = hex::encode(sha1);
+
+        self.data.insert(key, (is_vendor, value))
+    }
+}
